0a5eec6ab2b3affc368a98e47bf2eb11e3c034ae
Mike Jiang <jbjsnow@163.com>
2016-12-07 15:20:33 +0800

    Update models.py

--- app/models.py
+++ app/models.py
@@ -209,6 +209,7 @@ class Post(db.Model):
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    body_html = db.Column(db.Text)
     
     @staticmethod
     def generate_fake(count=100):
@@ -224,3 +225,13 @@ class Post(db.Model):
                      author=u)
             db.session.add(p)
             db.session.commit()
+    
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+                        'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+                        'h1', 'h2', 'h3', 'p'] 
+        target.body_html = bleach.linkify(bleach.clean(
+            markdown(value, output_format='html'),
+            tags=allowed_tags, strip=True))
+        
+db.event.listen(Post.body, 'set', Post.on_changed_body)
