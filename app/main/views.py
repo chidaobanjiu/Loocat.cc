b08c9ab37d8caad818ce6ff14106add7bc45c6ad
Mike Jiang <jbjsnow@163.com>
2016-12-08 14:52:31 +0800

    Update views.py

--- app/main/views.py
+++ app/main/views.py
@@ -166,3 +166,17 @@ def followed_by(username):
     return render_template('followers.html', user=user, title="Followed of",
                           endpoint='.followed_by', pagination=pagination,
                           follows=follows) 
+
+@main.route('/all')
+@login_required
+def show_all():
+    resp = make_response(redirect(url_for('.index')))
+    resp.set_cookie('show_followed', '', max_age=30*24*60*60)
+    return resp
+
+@main.route('/followed')
+@login_required
+def show_followed():
+    resp = make_response(redirect(url_for('.index')))
+    resp.set_cookie('show_followed', '1', max_age=30*24*60*60)
+    return resp
