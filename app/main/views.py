b22a8241dc5d5567b3ef1ee17a53c95e2f1354a4
Mike Jiang <jbjsnow@163.com>
2016-12-08 14:46:49 +0800

    Update views.py

--- app/main/views.py
+++ app/main/views.py
@@ -17,11 +17,19 @@ def index():
         db.session.add(post)
         return redirect(url_for('.index'))
     page = request.args.get('page', 1, type=int)
+    show_followed = False
+    if current_user.is_authenticated:
+        show_followed = bool(request.cookies.get('show_followed', ''))
+    if show_followed:
+        query = current_user.followed_posts
+    else:
+        query = Post.query
     pageination = Post.query.order_by(Post.timestamp.desc()).paginate(
         page, per_page=current_app.config['FLASKY_POSTS_PRE_PAGE'],
         error_out=False)
     posts = pagination.items
-    return render_template('index.html', form=form, posts=posts, pagination=pagination)
+    return render_template('index.html', form=form, posts=posts, 
+                           show_followed=show_followed, pagination=pagination)
 
 
 @main.route('/user/<username>')
