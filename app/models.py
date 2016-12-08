a1e9699fafc82e29ee36e54be45887937c386974
Mike Jiang <jbjsnow@163.com>
2016-12-08 15:17:45 +0800

    Update models.py

--- app/models.py
+++ app/models.py
@@ -103,7 +103,15 @@ class User(UserMixin, db.Model):
                 db.session.commit()
             except IntegrityError:
                 db.session.rollback()
-
+    
+    @staticmethod
+    def add_self_follows():
+        for user in User.query.all():
+            if not user.is_following(user):
+                user.follow(user)
+                db.session.add(user)
+                db.session.commit()
+    
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
         if self.role is None:
@@ -114,7 +122,8 @@ class User(UserMixin, db.Model):
         if self.email is not None and self.avatar_hash is None:
             self.avatar_hash = hashlib.md5(
                 self.email.encode('utf-8')).hexdigest()
-
+            self.followed.append(Follow(followed=self))
+            
     @property
     def password(self):
         raise AttributeError('password is not a readable attribute')
