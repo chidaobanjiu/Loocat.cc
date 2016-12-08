70436247aae1e2baebad09dc88fd8dcbd39473bb
Mike Jiang <jbjsnow@163.com>
2016-12-08 16:26:55 +0800

    Update views.py

--- app/main/views.py
+++ app/main/views.py
@@ -197,3 +197,33 @@ def show_followed():
     resp = make_response(redirect(url_for('.index')))
     resp.set_cookie('show_followed', '1', max_age=30*24*60*60)
     return resp
+
+@main.route('/moderate')
+@login_required
+@permission_required(Permission.MODERATE_COMMENTS)
+def moderate():
+    page = request.args.get('page', 1, type=int)
+    pagination = Comment.query.order_by(Comment.timestamp.desc()).paginate(
+        page, per_page=current_app.config['FALSKY_COMMENTS_PER_PAGE'],
+        error_out=False)
+    comments = pagination.items
+    return render_template('moderate.html', comments=comments,
+                           pagination=pagination, page=page) 
+                                
+@main.route('/moderate/enable/<int:id>')
+@login_required
+@permission_required(Permission.MODERATE_COMMENTS)
+def moderate_enable(id):
+    comments = Comments.query.get_or_404(id)
+    comments.disabled = False
+    return redirect(url_for('.moderate',
+                    page=request.args.get('page', 1, type=int))) 
+                                
+@main.route('/moderate/disable/<int:id>')
+@login_required
+@permission_required(Permission.MODERATE_COMMENTS)
+def moderate_disable(id):
+    comments = Comments.query.get_or_404(id)
+    comments.disabled = True
+    return redirect(url_for('.moderate',
+                    page=request.args.get('page', 1, type=int))) 
