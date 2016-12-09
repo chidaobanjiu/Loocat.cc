24f79e1bf6e16ab79b4afdcc899ae4f6af083538
Mike Jiang <jbjsnow@163.com>
2016-12-09 17:59:56 +0800

    Update models.py

--- app/models.py
+++ app/models.py
@@ -2,8 +2,10 @@ from datetime import datetime
 import hashlib
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app, request
+from markdown import markdown
+from flask import current_app, request, url_for
 from flask_login import UserMixin, AnonymousUserMixin
+from app.exception import ValidationError
 from . import db, login_manager
 
 
