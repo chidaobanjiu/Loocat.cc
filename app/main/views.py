7fd7478fd79248b604976d6ce9a009812acd8f5a
Mike Jiang <jbjsnow@163.com>
2016-12-13 16:03:02 +0800

    Update views.py

--- app/main/views.py
+++ app/main/views.py
@@ -227,3 +227,13 @@ def moderate_disable(id):
     comments.disabled = True
     return redirect(url_for('.moderate',
                     page=request.args.get('page', 1, type=int))) 
+                                
+@main.route('/shutdown')
+def server_shutdown():
+    if not current_app.testing:
+        abort(404)
+    shutdown = request.environ.get('werkzeug.server.shutdown')
+    if not shutdown:
+        abort(500)
+    shutdown()
+    return 'shutting down...'
