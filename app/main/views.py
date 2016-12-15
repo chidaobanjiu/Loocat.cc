fac967f4a22a437248ec562f4a25f0184a34afea
Mike Jiang <jbjsnow@163.com>
2016-12-15 10:14:38 +0800

    Update views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from flask import render_template, redirect, url_for, abort, flash
 from flask_login import login_required, current_user
+from flask_sqlalchemy import get_debug_queries
 from . import main
 from .forms import EditProfileForm, EditProfileAdminForm, PostForm
 from .. import db
@@ -237,3 +238,12 @@ def server_shutdown():
         abort(500)
     shutdown()
     return 'shutting down...'
+
+@main.after_app_request
+def after_request(response):
+     for query in get_debug_queries():
+          if query.duration >= current_app.config['FLASKY_SLOW_DB_QUERY_TIME']
+              current_app.logger.warning(
+                  'Slow query: %s\nParameters: %s\nDuration: %fs\nContext: %s\n' %(
+                      query.statement, query.parameters, query.duration, query.context))
+          return response
