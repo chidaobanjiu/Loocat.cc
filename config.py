876b6446d8f7ce7a22df9cda80ff97dc1611f841
Mike Jiang <jbjsnow@163.com>
2016-12-23 14:20:22 +0800

    Update config.py

--- config.py
+++ config.py
@@ -41,8 +41,29 @@ class TestingConfig(Config):
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-
-
+    
+    @classmethed
+    def init__app(cls, app):
+        Config.init__app(app)
+        
+        import logging
+        from logging.handlers import SMTPHandler
+        credentials = None
+        secure = None
+        if getattr(cls, 'MAIL_USERNAME', None) is not None:
+            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+            if getattr(cls, 'MAIL_USE_TLS', None):
+                secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+            fromaddr=cls.FLASKY_MAIL_SENDER,
+            toaddrs=[cls.FLASKY_ADMIN],
+            subjuct=cls.FLASKY_MAIL_SUBJECT_PREFIX + ' APPLICATION Error',
+            credentials=credentials,
+            secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+    
 config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
