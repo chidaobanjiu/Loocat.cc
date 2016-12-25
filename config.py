549bc4d0b96133b930db0e9d803129d22ff6b5c6
chidaobanjiu <jbjsnow@163.com>
2016-12-25 17:19:40 +0800

    解决用户权限问题及sqlalchemy错误提示
    包括首页不显示post模块，及用户不是管理员的问题

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_RECORD_QUERIES = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
     MAIL_SERVER = 'smtp-mail.outlook.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
